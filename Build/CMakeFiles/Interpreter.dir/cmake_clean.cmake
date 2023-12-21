7d26cd65c0fb745c9923c4932cfe7ebd19765ea7
RevisedJDUDE2 <JDUDEofficial134@gmail.com>
2023-12-21 17:09:17 +0800

    removed semicolon on cmake and configure project

---
+++ Build/CMakeFiles/Interpreter.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Interpreter.dir/main.cpp.obj"
+  "CMakeFiles/Interpreter.dir/main.cpp.obj.d"
+  "CMakeFiles/Interpreter.dir/src/SILangMem.cpp.obj"
+  "CMakeFiles/Interpreter.dir/src/SILangMem.cpp.obj.d"
+  "Interpreter.exe"
+  "Interpreter.exe.manifest"
+  "Interpreter.pdb"
+  "libInterpreter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Interpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Build/CMakeFiles/Interpreter.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Interpreter.
+# This may be replaced when dependencies are built.
