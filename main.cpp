d53d51e7f991fa8ddb13b334e260494d7c39a95a
RevisedJDUDE2 <JDUDEofficial134@gmail.com>
2023-12-22 16:40:21 +0800

    Changed main

--- main.cpp
+++ main.cpp
@@ -11,5 +11,10 @@ int main() {
   Stack.AssignVariables({0, 0, 104, 0, "Hello World"}, "h");
   VariableInfo* VI = Stack.GetAllVariableInfo();
   std::cout << "Name: " << VI[Stack.GetLastAssigned()].name << "\n";
+  while(true) {
+   if(GetAsyncKeyState(VK_ESCAPE)) {
+    break;
+   } 
+  }
   return 0;
 }
